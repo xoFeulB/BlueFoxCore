c17f43ee6d79a7a2c86c5cbda8b1e1f5a52ee65c
はまち <31212444+HeyHamachi@users.noreply.github.com>
2024-12-17 07:38:30 +0900

    Update define.js

--- extension/Chrome/src/js/modules/Values/define.js
+++ extension/Chrome/src/js/modules/Values/define.js
@@ -1,7 +1,7 @@
 export let DEFINE = (async () => {
   let R = {
     Title: `^.,.^ BlueFox`,
-    Copyright: `© ${new Date().getFullYear()} BlueFoxEnterprise, Inc.`,
+    Copyright: `© ${new Date().getFullYear()} BlueFoxEnterprise`,
     Version: `v${chrome.runtime.getManifest().version}`,
   };
   let config = await (await fetch("/src/json/config.json")).json();
